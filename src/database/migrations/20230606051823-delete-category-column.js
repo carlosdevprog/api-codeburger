33a433e0cef93dec3d2d9a4dcbf54fd24fe87828
Carlos Eduardo Lima <carloseduardolima.dev@gmail.com>
2023-06-18 10:44:10 -0300

    first commit

---
+++ src/database/migrations/20230606051823-delete-category-column.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface) => {
+
+    await queryInterface.removeColumn('products', 'category');
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+    await queryInterface.createColumn('products', {
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+
+    })
+
+  }
+}
+
